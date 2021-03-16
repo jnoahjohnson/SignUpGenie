83598c0fb9d7732524799aad40f0290ef535b75c
Noah Johnson <jnoahjohnson@gmail.com>
2021-03-16 16:00:53 -0600

    Add view models

---
+++ SignUpGenie/Models/ViewModels/TourListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpGenie.Models.ViewModels
+{
+    public class TourListViewModel
+    {
+        public IEnumerable<Tour> Tours { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
