57ea26c2d62b9f278999254a2b06cc06b60287d0
Noah Johnson <jnoahjohnson@gmail.com>
2021-03-15 18:42:49 -0600

    Fixed repo

---
+++ SignUpGenie/Models/EFGenieRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpGenie.Models
+{
+    public class EFGenieRepository : IGenieRepository
+    {
+        // Create a context from GenieDbContext
+        private GenieDbContext _context;
+
+        // Constructor
+        public EFGenieRepository (GenieDbContext context)
+        {
+            _context = context;
+        }
+
+        // Requirements from the interface - basically the properties we need to have
+        public IQueryable<Group> Groups => _context.Groups;
+        public IQueryable<Tour> Tours => _context.Tours;
+
+    }
+}
